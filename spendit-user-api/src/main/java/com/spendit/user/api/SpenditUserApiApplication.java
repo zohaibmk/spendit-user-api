16e7ae8a946766d1e73e04a7c3f43e9a6b3be1d3
Zohaib Masood <zohaib.masood@stud.th-deg.de>
2021-04-05 21:51:47 +0200

    Setting Spring Boot Project.

---
+++ spendit-user-api/src/main/java/com/spendit/user/api/SpenditUserApiApplication.java
@@ -0,0 +1,13 @@
+package com.spendit.user.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpenditUserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpenditUserApiApplication.class, args);
+	}
+
+}
---
+++ spendit-user-api/src/test/java/com/spendit/user/api/SpenditUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spendit.user.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpenditUserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
